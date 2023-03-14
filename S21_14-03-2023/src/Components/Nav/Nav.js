92c37bc9d09b851a162ca6a19b07e558aef2815d
Kabir <sagithedeveloper@gmail.com>
2023-03-14 08:49:04 +0530

    Added code

---
+++ S21_14-03-2023/src/Components/Nav/Nav.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./Nav.css"
+function Nav(props) {
+    return (
+        <div className='nav'>
+            <div className='title'>
+                <h2>React Component</h2>
+            </div>
+            <div className='menu'>
+                <a href="#">Home</a>
+                <a href="#">Counter</a>
+                <a href="#">Profile</a>
+                <a href="">Support</a>
+            </div>
+        </div>
+    );
+}
+
+export default Nav;
